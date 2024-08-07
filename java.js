03c2bc50aa9fed78423160299e84d6f7310af5cc
Alok kumar jha <aalok3828@gmail.com>
2024-08-07 13:22:33 +0530

    fixed somebugs

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
 //   toggle icon navbar
- let menuIcon = document.querySelector('#menu-icon');
- let navbar = document.querySelector('.navbar');
-   
- menuIcon.onclick = ()  => {
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
- };
-  /*=================== scroll sections active link ====================*/
+};
+/*=================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -15,14 +15,14 @@ window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let height =sec.offsetHeight;
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-             navLinks.forEach(links => {
-                  links.classList.remove('active');
-                  document.querySelector('header nav a[href*='  +  id  + ']').classList.add('active');
-             });
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
         };
 
     });
@@ -30,29 +30,30 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
-     
+
     // remove toggle icon and navbar when click navbar link (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
 // scroll reveal
-  ScrollReveal({
+ScrollReveal({
     // rest: true,
-    distance :'80px',
+    distance: '80px',
     duration: 2000,
     delay: 200
 });
 
-ScrollReveal().reveal('.home-content, .heading',{origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{origin: 'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img ',{origin: 'left'});
-ScrollReveal().reveal('.home-content p, .about-content ',{origin: 'right'});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right' });
 
 // typed js 
-const typed = new Typed('.multiple-text',{
-    strings: ['Frontend Devloper', 'Web Designer', 'cybersecurity'],
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Devloper', 'Web Devloper'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 });
+// contact email 
